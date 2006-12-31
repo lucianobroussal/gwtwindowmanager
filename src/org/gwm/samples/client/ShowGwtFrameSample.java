28503f0754b24f03fc2cc8b8530bcc5196e7581a
johanlodgon <johanlodgon@6d4f3f97-651d-0410-8897-8b004792cadb>
2006-12-31 13:32:13 +0000

    add sample

---
+++ src/org/gwm/samples/client/ShowGwtFrameSample.java
@@ -0,0 +1,21 @@
+package org.gwm.samples.client;
+
+import com.google.gwt.core.client.*;
+import com.google.gwt.user.client.*;
+import com.google.gwt.user.client.ui.*;
+import org.gwm.client.*;
+import org.gwm.client.impl.*;
+
+public class ShowGwtFrameSample implements EntryPoint {
+
+  private GDesktopPane pane;
+
+  public void onModuleLoad() {
+    pane = new GDesktopPane();
+    GInternalFrame gif = new GwtInternalFrame();
+    gif.setContent (new Button ("a button"));
+    gif.show(true);
+    // pane.addGInternalFrame (gif);
+  }
+
+}
