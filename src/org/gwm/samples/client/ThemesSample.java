d2bbc1b0dab10416f84529cbf16a9c4d62e53f10
luciano.broussal <luciano.broussal@6d4f3f97-651d-0410-8897-8b004792cadb>
2007-01-23 17:47:07 +0000

    samples : desktop , themes , dialogs

---
+++ src/org/gwm/samples/client/ThemesSample.java
@@ -0,0 +1,70 @@
+package org.gwm.samples.client;
+
+import org.gwm.client.impl.GwtInternalFrame;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ChangeListener;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class ThemesSample implements EntryPoint {
+
+    private ListBox listBox;
+
+    private GwtInternalFrame frame;
+
+    public void onModuleLoad() {
+
+        testThemes();
+    }
+
+    private void testThemes() {
+        listBox = new ListBox();
+        listBox.addItem("default", "default");
+        listBox.addItem("theme1", "theme1");
+        listBox.addItem("alphacube", "alphacube");
+        listBox.addItem("spread", "spread");
+        listBox.addItem("alert", "alert");
+        listBox.addItem("darkX", "darkX");
+
+        listBox.addChangeListener(new ChangeListener() {
+
+            public void onChange(Widget sender) {
+                String theme = getCurrentTheme();
+                frame.setTheme(theme);
+
+            }
+
+        });
+        listBox.setEnabled(false);
+        RootPanel.get().add(listBox);
+
+        Button button = new Button("Test Frame  Theme");
+        button.addClickListener(new ClickListener() {
+
+            public void onClick(Widget sender) {
+                if (!listBox.isEnabled()) {
+                    listBox.setEnabled(true);
+                }
+                frame = new GwtInternalFrame("http://www.google.com");
+                frame.setUrl("http://www.google.com");
+                frame.setSize(300, 200);
+                frame.setLocation(200, 200);
+                frame.setTheme(getCurrentTheme());
+                frame.setVisible(true);
+            }
+
+        });
+        RootPanel.get().add(button);
+
+    }
+
+    private String getCurrentTheme() {
+        String theme = listBox.getItemText(listBox.getSelectedIndex());
+        return theme;
+    }
+
+}
