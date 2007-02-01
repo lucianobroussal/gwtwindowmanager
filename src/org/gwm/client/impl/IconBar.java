0758af665a604865df202b1b499d545e64849297
johanlodgon <johanlodgon@6d4f3f97-651d-0410-8897-8b004792cadb>
2007-02-01 20:18:03 +0000

    icons

--- src/org/gwm/client/impl/IconBar.java
+++ src/org/gwm/client/impl/IconBar.java
@@ -1,5 +1,6 @@
 package org.gwm.client.impl;
 
+import com.google.gwt.user.client.*;
 import com.google.gwt.user.client.ui.*;
 import org.gwm.client.*;
 import java.util.*;
@@ -8,25 +9,39 @@ public class IconBar extends FlowPanel {
 
     GDesktopPane parent;
     Map buttonFrame;
+    Map buttonIcon;
 
     public IconBar (GDesktopPane parent) {
         super ();
         this.parent = parent;
         this.buttonFrame = new HashMap();
+        this.buttonIcon = new HashMap();
     }
 
-    public void addWindow (GInternalFrame frame) {
-        Button button = new Button (frame.getCaption(), new ClickListener() {
-            public void onClick(Widget sender) {
+    public void addWindow (GInternalFrame gframe) {
+        GwtInternalFrame frame = (GwtInternalFrame)(gframe);
+        HorizontalPanel icon = new HorizontalPanel();
+        icon.addStyleName (frame.getTheme()+"_topBar_iconButton");
+        Label label = new Label (frame.getCaption());
+        label.addStyleName (frame.getTheme()+"_topBar_icon");
+        Label restoreButton = new Label("");
+        restoreButton.setStyleName(frame.getTheme() + "_topBar_restore");
+        icon.add (label);
+        icon.add (restoreButton);
+        buttonFrame.put (restoreButton, frame);
+        buttonIcon.put (restoreButton, icon);
+        this.add (icon);
+        restoreButton.addClickListener (new ClickListener() {
+            public void onClick (Widget sender) {
+System.out.println ("CLICKicon");
                 GInternalFrame myFrame = (GInternalFrame)buttonFrame.get (sender);
                 buttonFrame.remove (sender);
-                remove (sender);
+                HorizontalPanel hpicon = (HorizontalPanel)buttonIcon.get (sender);
+                remove (hpicon);
                 parent.deIconify(myFrame);
+              
             }
         });
-
-        this.buttonFrame.put (button, frame);
-        this.add (button);
     }
 
     public void adjustSize () {
