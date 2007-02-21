78c2ae097a2b2dbe73700bae86ac6e21b9476d65
luciano.broussal <luciano.broussal@6d4f3f97-651d-0410-8897-8b004792cadb>
2007-02-21 10:33:25 +0000

    GFrameAdapter

---
+++ src/org/gwm/client/event/GFrameAdapter.java
@@ -0,0 +1,29 @@
+package org.gwm.client.event;
+
+public class GFrameAdapter  implements GFrameListener{
+
+    public void frameClosed(GFrameEvent evt) {
+    }
+
+    public void frameIconified(GFrameEvent evt) {
+    }
+
+    public void frameMaximized(GFrameEvent evt) {
+     }
+
+    public void frameMinimized(GFrameEvent evt) {
+    }
+
+    public void frameMoved(GFrameEvent evt) {
+    }
+
+    public void frameOpened(GFrameEvent evt) {
+    }
+
+    public void frameResized(GFrameEvent evt) {
+    }
+
+    public void frameRestored(GFrameEvent evt) {
+    }
+
+}
