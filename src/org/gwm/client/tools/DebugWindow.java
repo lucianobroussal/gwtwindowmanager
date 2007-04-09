7e9151f55bb0477ee6c0e36bea87c10bbb2481b6
luciano.broussal <luciano.broussal@6d4f3f97-651d-0410-8897-8b004792cadb>
2007-04-09 12:56:53 +0000

    pre 0.6

--- src/org/gwm/client/tools/DebugWindow.java
+++ src/org/gwm/client/tools/DebugWindow.java
@@ -25,18 +25,16 @@ import org.gwm.client.event.GFrameEvent;
 import org.gwm.client.event.GFrameListener;
 import org.gwm.client.impl.DefaultGFrame;
 
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.DockPanel;
-import com.google.gwt.user.client.ui.FiresFormEvents;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
-public class DebugWindow implements GFrameListener {
+public class DebugWindow {
 
     private DockPanel dock;
 
@@ -45,6 +43,8 @@ public class DebugWindow implements GFrameListener {
     private Button clearLog;
 
     private GFrame frame;
+    
+     
 
     public DebugWindow() {
         frame = new DefaultGFrame("Debug Window");
@@ -74,57 +74,76 @@ public class DebugWindow implements GFrameListener {
         this.dock.add(panel, DockPanel.CENTER);
     }
 
-    public void frameResized(GFrameEvent evt) {
-        addEvent(evt, "Resized");
-    }
-
-    public void frameOpened(GFrameEvent evt) {
-        addEvent(evt, "Opened");
-    }
-
-    public void frameClosed(GFrameEvent evt) {
-        addEvent(evt, "Closed");
-    }
-
-    public void frameMaximized(GFrameEvent evt) {
-        addEvent(evt, "Maximize");
-    }
-
-    public void frameMinimized(GFrameEvent evt) {
-        addEvent(evt, "Minimized");
-    }
-
-    public void frameIconified(GFrameEvent evt) {
-        addEvent(evt, "Iconified");
-    }
-
-    public void frameRestored(GFrameEvent evt) {
-        addEvent(evt, "Restored");
-    }
-
-    public void frameMoved(GFrameEvent evt) {
-        addEvent(evt, "Moved");
-    }
-    public void frameMoving(GFrameEvent evt) {
-    	addEvent(evt, "Moving");
-    }
+   
 
     private void addEvent(GFrameEvent evt, String action) {
+        if(panel.getWidgetCount() % 10 == 0){
+            clearPanel();
+        }
+        
         final GFrame myFrame = evt.getGFrame();
         String title = myFrame.getCaption();
         Label l = new Label(action + " " + title);
         l.addStyleName("gwm-DebugEvent");
-        l.addClickListener(new ClickListener() {
-            public void onClick(Widget sender) {
-                // myFrame.toFront();
-            }
-        });
         this.panel.add(l);
         ((DefaultGFrame)frame).fireFrameResized();
     }
 
     public GFrame getUI() {
         return frame;
+    }
+
+    public GFrameListener getFrameListener() {
+       return  new GFrameListener(){
+            public void frameResized(GFrameEvent evt) {
+                addEvent(evt, "Resized");
+            }
+
+            public void frameOpened(GFrameEvent evt) {
+                addEvent(evt, "Opened");
+            }
+
+            public void frameClosed(GFrameEvent evt) {
+                addEvent(evt, "Closed");
+            }
+
+            public void frameMaximized(GFrameEvent evt) {
+                addEvent(evt, "Maximize");
+            }
+
+            public void frameMinimized(GFrameEvent evt) {
+                addEvent(evt, "Minimized");
+            }
+
+            public void frameIconified(GFrameEvent evt) {
+                addEvent(evt, "Iconified");
+            }
+
+            public void frameRestored(GFrameEvent evt) {
+                addEvent(evt, "Restored");
+            }
+
+            public void frameMoved(GFrameEvent evt) {
+                addEvent(evt, "Moved");
+            }
+            public void frameMoving(GFrameEvent evt) {
+                addEvent(evt, "Moving");
+            }
+            
+            public void frameSelected(GFrameEvent evt) {
+                addEvent(evt, "Selected");
+            }
+
+            public void frameGhostMoving(int top, int left, GFrameEvent evt) {
+                addEvent(evt , "Ghost moving");
+            }
+
+            public void frameGhostMoved(int top, int left, GFrameEvent evt) {
+                addEvent(evt , "Ghost moved");
+                
+            }
+
+        };
     }
 
 }
