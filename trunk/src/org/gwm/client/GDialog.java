513d27e259de1587ff1f82272991033dd63a23e5
luciano.broussal <luciano.broussal@6d4f3f97-651d-0410-8897-8b004792cadb>
2007-04-09 12:55:42 +0000

    pre 0.6

--- trunk/src/org/gwm/client/GDialog.java
+++ trunk/src/org/gwm/client/GDialog.java
@@ -16,42 +16,53 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
+ *  
  */
 package org.gwm.client;
 
 import org.gwm.client.event.GDialogChoiceListener;
+import org.gwm.client.impl.DefaultGDialog;
 import org.gwm.client.impl.DefaultGDialog.Option;
+
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.UIObject;
 
 /**
  * Provides the behavior handles to interact with a DefaultGDialog.
  */
 public interface GDialog extends GFrame {
 
-    public static final int ERROR_MESSAGE = 1;
+    public final int ERROR_MESSAGE = 1;
 
-    public static final int INFORMATION_MESSAGE = 2;
+    public final int INFORMATION_MESSAGE = 2;
 
-    public static final int WARNING_MESSAGE = 3;
+    public final int WARNING_MESSAGE = 3;
 
-    public static final int QUESTION_MESSAGE = 4;
+    public final int QUESTION_MESSAGE = 4;
 
-    public static final int PLAIN_MESSAGE = 5;
+    public final int PLAIN_MESSAGE = 5;
 
-    public static final int OK_OPTION_TYPE = 4;
+    public final int OK_OPTION_TYPE = 4;
 
-    public static final int YES_NO_OPTION_TYPE = 5;
+    public final int YES_NO_OPTION_TYPE = 5;
 
-    public static final int YES_NO_CANCEL_OPTION_TYPE = 6;
+    public final int YES_NO_CANCEL_OPTION_TYPE = 6;
 
-    public static final int OK_CANCEL_OPTION_TYPE = 7;
-    
-    public static final Option OK_OPTION = new Option("Ok");
+    public final int OK_CANCEL_OPTION_TYPE = 7;
 
-    public static final Option YES_OPTION = new Option("Yes");
+    public final Option OK_OPTION = new Option("Ok");
 
-    public static final Option NO_OPTION = new Option("No");
+    public final Option YES_OPTION = new Option("Yes");
 
-    public static final Option CANCEL_OPTION = new Option("Cancel");
+    public final Option NO_OPTION = new Option("No");
+
+    public final Option CANCEL_OPTION = new Option("Cancel");
+
+    public static final String YES_OPTION_LABEL = "Yes";
+
+    public static final String NO_OPTION_LABEL = "No";
+
+    public static final String CANCEL_OPTION_LABEL = "Cancel";
 
     /**
      * Shows the DefaultGDialog.
@@ -61,10 +72,41 @@ public interface GDialog extends GFrame {
 
     public void setMessageType(int messageType);
 
-    public void setOptionType(int optionsType);
+    public int getMessageType();
+
+    public void setOptions(int optionType, Object[] options);
+
+    public Object[] getOptions();
+
+    public int getOptionType();
 
     public void setGDialogChoiceListener(GDialogChoiceListener choiceListener);
 
     public void setMessage(Object message);
+
+    public Object getMessage();
+
+    public Object getSelectedValue();
+
+    public Object getSelectedOption();
+
+    public void setParent(UIObject parent);
+
+    public void setIcon(Image icon);
+
+    public Image getIcon();
+
+    public void setSelectionValues(Object[] selectionValues);
+
+    public Object[] getSelectionValues();
+
+    public void setInitialValue(Object initialValue);
+
+    public Object getInitialValue();
+
+    public void setValue(Object value);
+
+    public Object getValue();
+    
 
 }
--- trunk/src/org/gwm/client/GInternalFrame.java
+++ trunk/src/org/gwm/client/GInternalFrame.java
@@ -35,4 +35,8 @@ public interface GInternalFrame extends GFrame {
      */
     public void setDesktopPane(GDesktopPane desktopPane);
 
+    
+
+
+
 }
